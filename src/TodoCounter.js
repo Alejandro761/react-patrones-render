124072608fba28dd2119912d5429c3d2c7feb9c8
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-29 20:59:09 -0500

    css en react. 7

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './TodoCounter.css';
 
-const styles = {
-  color: 'white',
-  backgroundColor: 'black',
-};
+// const styles = {
+//   color: 'white',
+//   backgroundColor: 'black',
+//  }; este objeto se puede pasar como los estilos
 
 function TodoCounter() {
     return (
-      <h2 style={styles}>Has completado 2 de 3 TODOs</h2>
+      // <h2 style={styles}>Has completado 2 de 3 TODOs</h2> una forma de agregar css en react es ponerle style y pasarle un objeto
+      <h2 className='TodoCounter' >Has completado 2 de 3 TODOs</h2>
     );
 }
 
