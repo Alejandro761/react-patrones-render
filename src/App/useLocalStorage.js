5c2c980e52665dc0bb311a2e12b1ac4a0917105d
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-07-16 20:12:10 -0500

    esta parte pertenece al curso de manejo del estado con react: migracion de useState a useReducer en useLocalStorage. 17

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 const useLocalStorage = (itemName, initialValue) => {
-  const [sincronizedItem, setSincronizedItem] = React.useState(true);
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
+  const [state, dispatch] = React.useReducer(reducer, initialState({initialValue}));
 
+  const {
+    sincronizedItem,
+    error,
+    loading,
+    item
+  } = state;
+
+  const onError = (error) => dispatch({type: actionTypes.error, payload: error});
+  
+  const onSuccess = (item) => { 
+    dispatch({type: actionTypes.success, payload: item})
+  };
+  
+  const onSave = (item) => { 
+    dispatch({type: actionTypes.save, payload: item})
+  };
+  
+  const onSincronize = () => dispatch({type: actionTypes.sincronize});
+  
   React.useEffect(() => {
     setTimeout(() => {
       try {
@@ -19,11 +35,13 @@ const useLocalStorage = (itemName, initialValue) => {
           parsedItem = JSON.parse(localStorageItem);
         }
 
-        setItem(parsedItem);
-        setLoading(false);
-        setSincronizedItem(true);
+        onSuccess(parsedItem);
+        // setItem(parsedItem);
+        // setLoading(false);
+        // setSincronizedItem(true);
       } catch (error) {
-        setError(error);
+        onError(error);
+        // setError(error);
       }
       
     }, 1000);
@@ -33,17 +51,60 @@ const useLocalStorage = (itemName, initialValue) => {
     try {
       const stringifiedItem = JSON.stringify(newItem);
       localStorage.setItem(itemName, stringifiedItem); //actualizacion en local storage
-      setItem(newItem); //actualizacion en el estado de react
+      onSave(newItem);
+      // setItem(newItem); //actualizacion en el estado de react
     } catch (error) {
-      setError(error);
+      onError(error);
+        // setError(error);
     }
   };
 
   const sincronizeItem = () => {
-    setLoading(true);
-    setSincronizedItem(false);
+    onSincronize();
   }
+
   return {item, saveItem, loading, error, sincronizeItem};
+}
+
+const initialState = ({ initialValue }) => ({
+  sincronizedItem : true,
+  error : false,
+  loading : true,
+  item : initialValue,
+});
+
+const actionTypes = {
+  error: 'ERROR',
+  success: 'SUCCESS',
+  save: 'SAVE',
+  sincronize: 'SINCRONIZE'
+};
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.error] : {
+    ...state,
+    error: true
+  },  
+  [actionTypes.success] : {
+    ...state,
+    error: false,
+    item: payload,
+    sincronizedItem: true,
+    loading: false,
+  },  
+  [actionTypes.save] : {
+    ...state,
+    item: payload,
+  },  
+  [actionTypes.sincronize] : {
+    ...state,
+    loading: true,
+    sincronizedItem: false,
+  },  
+})
+
+const reducer = (state, action) => {
+  return reducerObject(state, action.payload)[action.type] || state;
 }
 
 export {useLocalStorage};
